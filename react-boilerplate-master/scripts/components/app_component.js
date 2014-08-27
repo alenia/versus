1472b6aacb16648ae2b0d084b3843d987669cad6
dcsan <dc@pikkle.com>
2014-08-26 21:20:18 -0700

    components editable

--- react-boilerplate-master/scripts/components/app_component.js
+++ react-boilerplate-master/scripts/components/app_component.js
@@ -8,7 +8,8 @@ module.exports = React.createClass({
   },
 
   setAttributes: function (attributes) {
-    this.props.player.setAttributes(this.state.attributes);
+    console.log("setAttributes", attributes);
+    this.props.player.setAttributes(attributes);
   },
 
   setLearnerState: function (learnerState) {
@@ -36,9 +37,69 @@ module.exports = React.createClass({
     this.props.player.on('attributesChanged', this.onAttributesChanged.bind(this))
     this.props.player.on('learnerStateChanged', this.onLearnerStateChanged.bind(this))
     this.props.player.on('editableChanged', this.onEditableChanged.bind(this));
+    this.props.challengesApi = new VersalChallengesAPI( function() {
+      console.log("created challengesApi")
+    })
+
+  },
+
+  editMe: function(wot, event) {
+    var data;
+    if (this.state.attributes && this.state.attributes.data) {
+      data = this.state.attributes.data;  
+    } else {
+      console.log("initData");
+      data = {};
+    }
+
+    data[wot] = event.target.value;
+    // this.state.player.txt[idx] = event.target.value;
+    this.setAttributes({
+      data: data
+    })
+  },
+
+  getText: function(idx) {
+    // debugger;
+    if (this.state.attributes && this.state.attributes.data) {
+      res = this.state.attributes.data[idx];
+    } else {
+      return "----"
+    }
+   
+  },
+
+  setChallenges: function(answer) {
+    blob = [
+      {
+          prompt: 'What is the color of the sky?',
+          answers: 'blue',
+          scoring: 'strict'
+      }
+    ];
+    this.props.challengesApi.setChallenges(blob);
   },
 
   render: function() {
-    return <div>{this.state.editable ? "author" : "learn"}</div>;
+    // this.state.txt = "edit me";
+
+    // if {this.state.editable}
+    //   return <div>"editing"</div>
+    // else
+      return <div>
+        <div>
+          {this.state.editable ? "edit" : "play"}
+        </div>
+        <textarea 
+          onChange={this.editMe.bind(this, "f1")} 
+          value={ this.getText("f1") }
+        />
+        <textarea 
+          onChange={this.editMe.bind(this, "f2")}
+          value={ this.getText("f2") }
+        />
+
+    </div>
+
   }
 })
